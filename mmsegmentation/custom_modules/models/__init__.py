a1cd333222d9e55ef89a42330e67beb2b29c937a
yehna <yehna2907@github.com>
2024-12-02 01:49:43 +0900

    Add mmsegmentation codes

---
+++ mmsegmentation/custom_modules/models/__init__.py
@@ -0,0 +1,3 @@
+from .custom_model import ASPPHeadWithoutAccuracy, FCNHeadWithoutAccuracy, SegformerHeadWithoutAccuracy, EncoderDecoderWithoutArgmax
+
+__all__ = ['ASPPHeadWithoutAccuracy', 'FCNHeadWithoutAccuracy', 'SegformerHeadWithoutAccuracy', 'EncoderDecoderWithoutArgmax']
